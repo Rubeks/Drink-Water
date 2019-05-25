c82ffbf5caf7eaf0dc4f81ca79a4c98ac341f08b
Rais Agliullov <rais-agliulov@yandex.ru>
2019-05-25 09:40:22 +0400

    Initial Commit

---
+++ Drink Water/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drink Water
+//
+//  Created by Раис Аглиуллов on 25/05/2019.
+//  Copyright © 2019 Раис Аглиуллов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
